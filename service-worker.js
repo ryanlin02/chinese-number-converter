0972aca435287777639a4e53c9ba29a90682051e
ryanlin02 <126218351+ryanlin02@users.noreply.github.com>
2025-03-03 23:49:40 +0800

    Update service-worker.js
    v0.56

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chinese-number-converter-v0.47';
+const CACHE_NAME = 'chinese-number-converter-v0.56';
 const ASSETS_TO_CACHE = [
   '/chinese-number-converter/',
   '/chinese-number-converter/index.html',
